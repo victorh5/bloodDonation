8103eb31774b8a6feafdcde33c7ccf1b56adb143
Victor Hugo <victor.gtts2@gmail.com>
2021-04-29 01:01:39 -0400

    commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Tela HTML feita por meio dos frameworks do JSON usando express, nodemon, nunjucks entre outros.
+Praticamente é um site para doação de sangue onde o interessado em fazer a doação, tem que colocar as informações de contato, e a empresa responsável neste atendimento
+dar este suporte para entrar em contato com o usuário.
+Aparecendo também os últimos doadores e seus tipos sanguíneos.
+Necessário node_modules
---
+++ server.js
@@ -0,0 +1,66 @@
+
+const express = require("express")
+const server = express()
+
+
+server.use(express.static('publico'))
+
+
+server.use(express.urlencoded({ extended: true }))
+
+const Pool = require('pg').Pool
+const db = new Pool({
+    user: 'postgres',
+    password: '123',
+    host: 'localhost',
+    port: 5432,
+    database: 'postgres'
+})
+
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("./", {
+    express: server,
+    noCache: true,
+})
+
+
+
+
+server.get("/", function (req, res) {
+    db.query("SELECT * FROM donors", function (err, result) {
+        if (err) return res.send("Erro no banco de dados.")
+        const donors = result.rows
+        return res.render("index.html", { donors })
+    })
+})
+
+
+server.post("/", function (req, res) {
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+
+    if (name == "" || email == "" || blood == "") {
+        return res.send("Todos os campos são obrigátorios!")
+    }
+
+
+    const query = `
+        INSERT INTO donors ("nome", "email", "blood")
+        VALUES ($1, $2, $3)`
+
+    const values = [name, email, blood]
+    db.query(query, values, function (err) {
+        if (err) return res.send("Erro no banco de dados.")
+
+        return res.redirect("/")
+    })
+
+})
+
+
+
+server.listen(4000, function () {
+    console.log("iniciei o server.")
+})
